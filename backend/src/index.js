be20f31ea70fe41e69ee9a324ac97fa027998896
Antonio <antoniodeveloper3@gmail.com>
2021-06-23 21:48:42 -0300

    Configurando Nodemon

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(request, response)=>{
+  return response.json({message:'Hello Wow 😋'});
+});
+
+app.listen(3333,()=>{
+  console.log('🚀Backend started!')
+});
