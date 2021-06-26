677e819233899ebe672a97f0c480b8c0db3491c2
Antonio <antoniodeveloper3@gmail.com>
2021-06-26 12:51:31 -0300

    Propiedades

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,29 @@ import React from "react";
 
 import Header from "./components/Header";
 
+/**
+ * Componente
+ * Propriedade
+ * Estado
+ */
+
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <Header title="Homepage">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header>
+    </>
   );
 }
 
