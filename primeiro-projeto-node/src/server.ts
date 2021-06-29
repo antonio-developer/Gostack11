590b91732102b1d31aa87ae7bb3829600e61ccbc
Antonio <antoniodeveloper3@gmail.com>
2021-06-29 11:39:13 -0300

    Eslint

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) =>{
-  return response.json({message:'Hello Go'});
-});
+app.get('/', (request, response) => response.json({ message: 'Hello Go' }));
 
-app.listen(3333, ()=>{
-  console.log('🚀 Server started on port 3333!')
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
 });
